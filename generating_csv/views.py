6a34902b393a0e5cea3d1f068891efdc2ce52b63
Timur <timur.hike@gmail.com>
2023-02-15 17:40:25 +0200

    Add SchemaListViews

--- generating_csv/views.py
+++ generating_csv/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from generating_csv.models import Schema
+
+
+@login_required
+def new_schema(request):
+    return render(request, "tesdad.html")
+
+
+class SchemaListViews(LoginRequiredMixin, generic.ListView):
+    model = Schema
+    fields = "__all__"
