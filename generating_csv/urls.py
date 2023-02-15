48567a085b2265c93557af2d246fbd87a688bf93
Timur <timur.hike@gmail.com>
2023-02-15 21:17:25 +0200

    Add CRUD for schema

--- generating_csv/urls.py
+++ generating_csv/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
-from generating_csv.views import new_schema, SchemaListViews
+from generating_csv.views import (
+    SchemaListViews,
+    SchemaDetailViews,
+    SchemaUpdateViews,
+    SchemaDeleteViews,
+    create_schema_and_columns,
+)
 
 urlpatterns = [
     path("", SchemaListViews.as_view(), name="schema-list"),
-    path("new_schema/", new_schema, name="new-schema"),
-
+    path("schema/creation/", create_schema_and_columns, name="schema-creation"),
+    path("schema/<int:pk>/", SchemaDetailViews.as_view(), name="schema-detail"),
+    path("schema/<int:pk>/update/", SchemaUpdateViews.as_view(), name="schema-update"),
+    path("schema/<int:pk>/delete/", SchemaDeleteViews.as_view(), name="schema-delete"),
 ]
 
 app_name = "generating_csv"
--- generating_csv/views.py
+++ generating_csv/views.py
@@ -1,16 +1,58 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy, reverse
 
-from generating_csv.models import Schema
+from generating_csv.forms import SchemaForms, ColumnForms
+from generating_csv.models import Schema, Column
 
 
 @login_required
-def new_schema(request):
-    return render(request, "tesdad.html")
+def create_schema_and_columns(request):
+    if request.method == "GET":
+        column = Column.objects.all()
+        context = {
+            "schema_forms": SchemaForms(),
+            "column_forms": ColumnForms(),
+            "column": column,
+        }
+        return render(request, "generating_csv/schema_creation.html", context=context)
+
+    if request.method == "POST":
+
+        columns_form = ColumnForms(request.POST)
+        if columns_form.is_valid():
+            columns_form.save()
+            return HttpResponseRedirect(reverse("generating_csv:schema-creation"))
+
+        schema_form = SchemaForms(request.POST)
+        if schema_form.is_valid():
+            print("asd")
+            schema_form.save()
+            return HttpResponseRedirect(reverse("generating_csv:schema-list"))
 
 
 class SchemaListViews(LoginRequiredMixin, generic.ListView):
     model = Schema
     fields = "__all__"
+
+
+class SchemaDetailViews(LoginRequiredMixin, generic.DetailView):
+    model = Schema
+    fields = "__all__"
+
+
+class SchemaUpdateViews(LoginRequiredMixin, generic.UpdateView):
+    model = Schema
+    fields = "__all__"
+    success_url = reverse_lazy("generating_csv:schema-list")
+
+
+class SchemaDeleteViews(LoginRequiredMixin, generic.DeleteView):
+    model = Schema
+    success_url = reverse_lazy("generating_csv:schema-list")
+
+
+
