fc2cee6fab9b62c290145eeace4e472cb04892b1
Timur <timur.hike@gmail.com>
2023-02-17 02:23:36 +0200

    Add CSVData in admin

--- generating_csv/admin.py
+++ generating_csv/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from generating_csv.models import User, Column, Schema
+from generating_csv.models import User, Column, Schema, CSVData
 
 admin.site.register(User)
 admin.site.register(Column)
 admin.site.register(Schema)
+admin.site.register(CSVData)
