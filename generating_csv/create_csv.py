59ddd129356efe5eec83034640e49ca05afc08fe
Timur <timur.hike@gmail.com>
2023-02-18 20:38:42 +0200

    Add create_csv.py

---
+++ generating_csv/create_csv.py
@@ -0,0 +1,44 @@
+import csv
+import uuid
+
+from generating_csv.models import CSVData
+from faker import Faker
+
+
+def generate_fake_data(columns):
+    fake = Faker()
+    fake_dict = {}
+
+    for column in columns:
+        type_faker_dic = {
+            "Full_name": f"{fake.first_name()} {fake.last_name()}",
+            "Job": fake.job(),
+            "Email": fake.email(),
+            "Phone_number": fake.phone_number(),
+            "Address": fake.address(),
+            "Integer": fake.random_int(),  # TODO: add start value & end value
+        }
+        fake_dict[column.name] = type_faker_dic[column.type]
+
+    return fake_dict
+
+
+def create_csv(rows, schema):
+    file_name = f"{schema.title}-{uuid.uuid4()}.csv"
+
+    columns = schema.columns.all()
+    field_names = [column.name for column in columns]
+
+    with open(f"media/{file_name}", "w", newline="") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=field_names)  # TODO: add sepatarot, string character
+        writer.writeheader()
+
+        for _ in range(int(rows)):
+            fake_dict = generate_fake_data(columns)
+            writer.writerow(fake_dict)
+
+    csv_data = CSVData.objects.create(schema=schema, rows=rows)
+    csv_data.csv_file.name = file_name
+    csv_data.save()
+
+    return csv_data
